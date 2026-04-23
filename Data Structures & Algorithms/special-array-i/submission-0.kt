5c30e01e3e371e3fa638a46ef6ff2060660a3505
Arindam Ghosh <me.aghosh@gmail.com>
2026-04-23 17:44:46 +0200

    Add: special-array-i - submission-0

---
+++ Data Structures & Algorithms/special-array-i/submission-0.kt
@@ -0,0 +1,9 @@
+class Solution {
+    fun isArraySpecial(nums: IntArray): Boolean {
+        for(i in 1 until nums.size){
+            if(nums[i]%2 == nums[i-1]%2)
+            return false
+        }
+        return true
+    }
+}
